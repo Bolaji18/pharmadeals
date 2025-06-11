c2121cfafcc0e9e4a8da0131eb39366d65d356fb
bolaji daropale <108666295+Bolaji18@users.noreply.github.com>
2025-06-11 13:10:49 +0100

    11 june 2025

--- pharmadeals/urls.py
+++ pharmadeals/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('daropaleadminpage7853663/', admin.site.urls),
     path('', include('pharma.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 
